87cd5e76f8310889b8f352ea433670dd1e617122
barry.guda <barry.guda@turnkeyafrica.com>
2021-10-26 07:47:57 +0300

    Testing on rails

--- hello_spec.rb
+++ hello_spec.rb
@@ -4,7 +4,7 @@ require_relative './hello_test'
 describe Greetings do
     describe "#sayhello" do
         it "should return Hello doing RSpec tests" do
-            Greetings.say_hello.must_equal 'Hello doing RSpec tests'
+            Greetings.say_hello.must_equal 'Hello doing RSpec tests' ##minitest can handle both TDD and BDD
         end
     end
 end
