ae976b032e4ed00977514e2128c6c5f136d69696
barry.guda <barry.guda@turnkeyafrica.com>
2021-12-01 23:05:54 +0300

    learnings

--- functions.rb
+++ functions.rb
@@ -49,3 +49,46 @@ def implicit_return(x)
 end
 puts implicit_return(9)
 #returns false
+
+# Example of a method definition that mutates its argument permanently
+a = [1, 2, 3]
+
+def mutate(array)
+    array.pop
+end
+p "Before mutate method: #{a}"
+p mutate(a) ##also returns the value returned by the mutate method
+p "After mutate method: #{a}"
+
+def add_three(number)
+    number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+###Chaining Methods###
+def  add_three(n)
+    new_value = n + 3
+    puts new_value
+    new_value
+end
+###returns 8
+add_three(5).times { puts 'this should print 8 times' } 
+
+#"hi there".length.to_s #returns 8 - a string
+
+##Method Calls as Arguments
+def add(a, b)
+    a + b
+end
+def subtract(a, b)
+    a - b
+end
+p add(20, 45)
+p subtract(80, 10)
+
+def multiply(num1, num2)
+    num1 * num2
+end
+p multiply(add(20, 45), subtract(80, 10))
--- variables.rb
+++ variables.rb
@@ -9,4 +9,46 @@ puts sum
 name = "Emily"
 age = 24
 
-puts "Your name is #{name} and you are #{age} years old"
+total = 0
+[1, 2, 3].each { |number| total +=number }
+puts total
+
+##both achieve the same thing
+total = 0
+[1, 2, 3].each do |number|
+    total += number
+end
+
+puts total
+
+arr = [1, 2, 3]
+
+for i in arr do
+    a = 5
+end
+puts a
+
+##getting user data
+puts "What is your name ?"
+name = gets.chomp
+#puts "Hello" + name
+for count in 1...10
+    puts "Hello #{name}"
+end
+#or
+##Times Method
+10.times do
+    puts "Hello #{name}"
+end
+
+##getting age
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In 10 years you wll be:"
+puts age + 10
+puts "In 20 years you will be:"
+puts age + 20
+puts "In 30 years you will be:"
+puts age + 30
+puts "In 40 years you will be:"
+puts age + 40
