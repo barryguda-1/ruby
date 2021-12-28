c86c9560ec55f334d981314c7650608218e4d1c3
barry.guda <barry.guda@turnkeyafrica.com>
2021-12-28 07:45:18 +0300

    Two-Sum

--- two_sum11.rb
+++ two_sum11.rb
@@ -1,3 +1,4 @@
+##Brute force fails some of the test-cases
 def two_sum(numbers, target)
     ##add validation
     return false if numbers.empty? == true
@@ -9,7 +10,24 @@ def two_sum(numbers, target)
             p idx_val = numbers.index(val)
         end
     end
-    print [numbers.index(numbers[0])+1,idx_val+1]
+    return [numbers.index(numbers[0])+1,idx_val+1]
 end
 
 two_sum([2,7,11,15],9)
+
+def two_sum(numbers,target)
+    return false if numbers.empty? == true
+    i = 0
+    j = numbers.length - 1
+
+    while i < j
+        sum = numbers[i] + numbers[j]
+        if sum == target
+            return [i + 1,j + 1] ##add one to cover the zero indexed constraint
+        elsif sum < target
+            i += 1
+        else
+            j -= 1
+        end
+    end
+end
