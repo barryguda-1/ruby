66e2e10b7af5ac642c3a5ce8e0cba7be4c2e81c8
barry.guda <barry.guda@turnkeyafrica.com>
2021-12-28 09:15:33 +0300

    better efficient solutin

--- leetcode_binarysearch.rb
+++ leetcode_binarysearch.rb
@@ -1,3 +1,4 @@
+#lame ruby solution
 def search(nums=[], target)
     if nums.include?(target)
          return nums.index(target)
@@ -7,3 +8,23 @@ def search(nums=[], target)
 end
 
 search([15, 7, 18, 5, 12, 8, 5, 1] , 24)
+
+
+##better efficient solution
+def search(nums, target)
+     i = 0
+     j = nums.length - 1
+
+     while i <= j
+          mid = i + (j - i)/2 ##best way to get the midpoint
+          
+          if nums[mid] == target
+               return mid
+          elsif nums[mid] > target
+               j = mid - 1
+          else
+               i = mid + 1
+          end
+     end
+     return -1
+end
